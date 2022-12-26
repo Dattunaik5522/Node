82a5a8ab8bb6991b823a45b9c991920345f00c44
Dattunaik5252 <gugulothdattu000@gmail.com>
2022-12-26 21:47:20 +0530

    About local module in Node

---
+++ add.js
@@ -0,0 +1,7 @@
+const add = (a, b) => {
+  return a + b;
+};
+
+console.log(add(10, 90));
+console.log(add(10, -90));
+console.log(add(10, -99999999));
---
+++ index.js
@@ -0,0 +1,3 @@
+require("./add.js"); //we can import any files which we wanna run
+
+console.log("Hello from index.js");
