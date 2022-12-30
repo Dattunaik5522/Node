7da7e3f762288cff0c5ca8c471f089df1477b545
Dattunaik5252 <gugulothdattu000@gmail.com>
2022-12-30 08:59:16 +0530

    export patterns

--- add.js
+++ add.js
@@ -2,8 +2,14 @@ const add = (a, b) => {
   return a + b;
 };
 
+const subtract = (a, b) => {
+  return a - b;
+};
 // console.log(add(10, 90));
 // console.log(add(10, -90));
 // console.log(add(10, -99999999));
 
-module.exports = add;
+module.exports = {
+  add: add,
+  subtract: subtract,
+};
--- index.js
+++ index.js
@@ -8,4 +8,15 @@ const addLogic = require("./add.js"); //we can import any files which we wanna r
  */
 
 console.log("Hello from index.js");
-console.log(addLogic(003, 297));
+console.log(addLogic.add(300, 297));
+console.log(addLogic.add(003, 297));
+console.log(addLogic.subtract(003, 297));
+console.log(addLogic.subtract(300, 297));
+/**
+ * Each loaded moddule in Node.js is wrapped with an IIFE that provides private scoping of code
+ * IIFE allows you to  repeat variable function names without any conflicts.
+ * MODULE WRAPPER
+ * --Every module in node.js gets wrapped in an IIFE before being loaded
+ * --IIFE helps keep top-level variables scoped to the module rather than the global object
+ * The IIFE that wraps every module contains 5 paramerters which are pretty important for the function module.
+ */
